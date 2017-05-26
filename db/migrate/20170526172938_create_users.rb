712174c9a28743e3a1addffac124d77e8dc21fb4
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-05-27 01:45:51 +0800

    user login and sign up done

---
+++ db/migrate/20170526172938_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+    	t.string :full_name, null: false
+    	t.string :email, unique: true, null: false
+    	t.string :password
+    	t.string :password_digest
+     	t.timestamps
+    end
+	    add_index :users, :full_name
+	    add_index :users, :email
+  end
+end
