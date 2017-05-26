4045f574e1ca078a4e08e0e29b982536a5f46810
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-05-27 02:06:04 +0800

    add bootstrap

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
 	  # Replace with code that works with your application
 	  def current_user
 	    if session[:user_id]
-	      @current_user ||= User.find_by_id(session[:user_id])
+	      @current_user ||= User.find(session[:user_id]) if session[:user_id]
 	    end
 	  end
 
