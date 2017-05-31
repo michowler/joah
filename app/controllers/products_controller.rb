cd0af35b335b33232ca5d04249910df4d4fedad4
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-06-01 00:28:03 +0800

    grant access

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-	#before_action :logged_in_user, only: [:new, :create]
-	before_action :require_login, only: [:new, :create]
+	
+	before_action :require_login, only: [:new, :create, :show]
 	before_action :find_product, only: [:show, :delete, :edit, :update]
 	#respond_to :html, :js
 	#https://launchschool.com/blog/the-detailed-guide-on-how-ajax-works-with-ruby-on-rails
